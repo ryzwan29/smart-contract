8de1a8f7f3c72345217942f62d9108aa3abdc1d6
Rizwan Fairuz Mamduh <rizwanfairuzmamduh29@gmail.com>
2024-12-07 21:20:52 +0700

    first commit

---
+++ contracts/Ryzwan.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+contract Ryzwan {
+    string public textRaw;
+    address private owner;
+    uint256 public angka;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    function setText(string calldata _text) external{
+        textRaw = _text;
+    }
+
+    function inc() external{
+        require(msg.sender == owner,"NOT_OWNER");
+        angka++;
+    }
+
+    function getPemilik() public view returns(address){
+        return owner;
+    }
+}
